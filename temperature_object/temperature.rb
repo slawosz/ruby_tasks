baefb88830bde30252a143584c605ba8a3992106
Barbara Slawinska <slawinska.barbara@gmail.com>
2013-07-26 17:55:02 +0200

    Nine tests have passed

--- temperature_object/temperature.rb
+++ temperature_object/temperature.rb
@@ -1,2 +1,18 @@
 class Temperature
+
+	def initialize (tempratures_hash)
+  		@tempratures = tempratures_hash
+	end
+
+
+	def in_fahrenheit
+		puts @temperatures
+  		@tempratures.key?(:f) ? @tempratures[:f] : (@tempratures[:c] * 9.0 / 5) + 32
+	end
+
+   	def in_celsius
+        @tempratures.key?(:c) ? @tempratures[:c] : (@tempratures[:f] - 32) * 5.0 / 9
+    end
+
+
 end
